458a3c1491bc65998841352ce792792b0d047e01
rahul shinde <rshinde@mica.edu>
2015-07-18 18:22:39 -0600

    got hover states to work

--- js/17-07-15-a.js
+++ js/17-07-15-a.js
@@ -2,6 +2,7 @@
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, 300 / 400, 0.1, 1000);
+var color
 
 camera.position.z = 100;
 
@@ -20,12 +21,30 @@ var light1 = new THREE.PointLight( 0xffffff, 1, 4500 );
 light1.position.set( 0, 10, 50 );
 scene.add( light1 );
 
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+function onDocumentMouseMove( event ) {
+
+	event.preventDefault();
+
+	if ($('#container1:hover').length > 0) {
+        color = '0xff0000';
+        console.log(color);
+    } else {
+    	color = '0x696969';
+    	console.log(color);
+    }
+
+}
+
 function render() {
 	requestAnimationFrame( render );
 
 	cube.rotation.x += 0.001;
 	cube.rotation.y += 0.01;
 	// cube.rotation.z += 0.01;
+
+	cube.material.emissive.setHex(color);
 
 	renderer.render( scene, camera );
 }
